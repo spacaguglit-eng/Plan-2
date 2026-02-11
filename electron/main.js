bbbd91eb0d74564ee761f67c597484931b1584ea
spacaguglit-eng <spacaguglit@gmail.com>
2026-02-11 09:22:55 +0300

    123

---
+++ electron/main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    show: false,
+    title: 'Планировщик смен',
+  });
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
